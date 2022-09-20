03e0373aadb910c6318e3236662a5d19b5b3e6bf
Anyx <anyxp@protonmail.com>
2022-09-20 12:02:32 +0300

    Create cccc.js

---
+++ cccc.js
@@ -0,0 +1,145 @@
+    PAGE.appGroups = [{"items":[
+
+{"height":"48","width":"48","searchtext":"password change secure cpanel Password Security","onclick":"","subtype":"img","acontent":"","file":"change_password","imgtype":"icon","itemorder":"1","target":"","group":"preferences","type":"image","implements":"Password_Change","url":"/panel/indexpl.php?option=passwordchange&ttt=-5815945509985899520","itemdesc":"Change Password","base64_png_image":"","feature":"password"},
+
+{"group":"preferences","target":"","itemorder":"2","imgtype":"icon","file":"change_language","feature":"setlang","base64_png_image":"","url":"/panel/indexpl.php?option=changelanguage&ttt=-5815945509985899520","itemdesc":"Change Language","type":"image","implements":"Locale_Change","searchtext":"language setlang Change Language","width":"48","height":"48","acontent":"","subtype":"img","onclick":""},
+
+{"base64_png_image":"","feature":"updatecontact","type":"image","implements":"ContactInfo_Change","url":"/panel/indexpl.php?option=changeemail&ttt=-5815945509985899520","itemdesc":"Update Contact Email","imgtype":"icon","itemorder":"4","target":"","group":"preferences","file":"contact_information","acontent":"","onclick":"","subtype":"img","width":"48","searchtext":"contact email Contact Information","height":"48"},
+
+
+{"base64_png_image":"","feature":"/images/help-icon-bar.png","type":"image","implements":"backup_wizard","url":"/panel/indexpl.php?option=gettingstarted&ttt=-5815945509985899520","itemdesc":"Getting Started","imgtype":"image","itemorder":"4","target":"","group":"preferences","file":"backup_wizard","acontent":"","onclick":"","subtype":"img","width":"48","searchtext":"contact email Contact Information","height":"48"},
+
+{"base64_png_image":"","feature":"/images/help-icon-bar.png","type":"image","implements":"backup_wizard","url":"/panel/indexpl.php?option=upgrade&ttt=-5815945509985899520","itemdesc":"Account Upgrades","imgtype":"image","itemorder":"4","target":"","group":"preferences","file":"two_factor_authentication","acontent":"","onclick":"","subtype":"img","width":"48","searchtext":"Upgrade Account","height":"48"},
+
+
+{"group":"preferences","imgtype":"icon","itemorder":"5","target":"","file":"user_manager","feature":"user_manager","base64_png_image":"","type":"image","url":"/panel/indexpl.php?option=accountsettings&ttt=-5815945509985899520","itemdesc":"Account Settings","searchtext":"account settings hostname ftp help","width":"48","height":"48","acontent":"","subtype":"img","onclick":""}],"desc":"Preferences","group":"preferences"},
+
+{"items":[
+
+{"file":"file_manager","imgtype":"icon","target":"file_manager","itemorder":"1","group":"files","type":"image","implements":"FileManager_Home","url":"http://filemanager.ai/new/#/c/185.27.134.11/fo2l2_22574995/eyJ0IjoiZnRwIiwiYyI6eyJ2IjowLCJwIjoidlJrdDVaNkpQUzRGQlkifX0=","itemdesc":"Online File Manager","base64_png_image":"","feature":"filemanager","height":"48","width":"48","searchtext":"File Manager file-manager","onclick":"","subtype":"img","acontent":"target=\"file_manager\""},
+
+{"base64_png_image":"","height":"48","width":"48","searchtext":"Directory Privacy passwordprotect password protect","onclick":"","subtype":"img","acontent":"","file":"directory_privacy","imgtype":"icon","target":"","itemorder":"3","group":"files","type":"image","itemdesc":"Directory Privacy","url":"\/panel\/indexpl.php?option=protectedfolders&ttt=-5815945509985899520","base64_png_image":"","feature":"webprotect"},
+
+{"feature":"diskusageviewer","base64_png_image":"","type":"image","url":"\/panel\/indexpl.php?option=diskspace&ttt=-5815945509985899520","itemdesc":"Disk Usage","group":"files","imgtype":"icon","itemorder":"4","target":"","file":"disk_usage","acontent":"","subtype":"img","onclick":"","searchtext":"Disk Usage disk space usage disk-usage","width":"48","height":"48"},
+
+{"touch":"ftp","height":"48","width":"48","searchtext":"FTP Accounts ftp create","onclick":"","subtype":"img","acontent":"","file":"ftp_accounts","imgtype":"icon","itemorder":"6","target":"","group":"files","type":"image","url":"\/panel\/indexpl.php?option=ftpsettings&ttt=-5815945509985899520","itemdesc":"FTP Accounts","base64_png_image":"","feature":"ftpaccts"},
+
+{"touch":"ftpsoftware","height":"48","width":"48","searchtext":"FTP clients ftp software filezilla ","onclick":"","subtype":"img","acontent":"","file":"ftp_connections","imgtype":"icon","itemorder":"6","target":"","group":"files","type":"image","url":"\/panel\/indexpl.php?option=ftpclients&ttt=-5815945509985899520","itemdesc":"Free FTP Software","base64_png_image":"","feature":"ftpaccts"},
+
+
+
+{"feature":"backup","base64_png_image":"","url":"\/panel\/indexpl.php?option=backup&ttt=-5815945509985899520","itemdesc":"Backups","implements":"Backups_Home","type":"image","group":"files","itemorder":"9","target":"","imgtype":"icon","file":"backup","acontent":"","subtype":"img","onclick":"","searchtext":"Backup restore","width":"48","height":"48"}],"group":"files","desc":"Files"},
+
+{"items":[
+
+{"searchtext":"phpMyAdmin database","width":"48","height":"48","acontent":"target=\"_blank\"","subtype":"img","onclick":"","group":"databases","imgtype":"icon","itemorder":"1","target":"_blank","file":"php_my_admin","feature":"phpmyadmin","base64_png_image":"","implements":"Database_phpMyAdmin","type":"image","url":"\/panel\/indexpl.php?option=pma&ttt=-5815945509985899520","itemdesc":"phpMyAdmin"},
+
+{"file":"mysql_databases","itemorder":"2","target":"","imgtype":"icon","group":"databases","itemdesc":"MySQL Databases","url":"\/panel\/indexpl.php?option=mysql&ttt=-5815945509985899520","type":"image","implements":"Database_MySQL","base64_png_image":"","feature":"mysql","height":"48","width":"48","searchtext":"MySQL Databases MySQL® Databases","onclick":"","subtype":"img","acontent":""},
+
+{"height":"48","searchtext":"Remote MySQL Remote MySQL®","width":"48","subtype":"img","onclick":"","acontent":"","file":"remote_mysql","group":"databases","imgtype":"icon","target":"","itemorder":"4","type":"image","itemdesc":"Remote MySQL","url":"\/panel\/indexpl.php?option=remotemysql&ttt=-5815945509985899520","feature":"mysql","base64_png_image":""}
+
+,{"onclick":"","subtype":"img","acontent":"","height":"48","width":"48","searchtext":"PostgreSQL Databases postgres database postgresql","itemdesc":"PostgreSQL Databases","url":"\/panel\/indexpl.php?option=psql&ttt=-5815945509985899520","type":"image","base64_png_image":"","feature":"postgres","file":"postgresql_databases","target":"","itemorder":"5","imgtype":"icon","group":"databases","if":"$haspostgres"}],"desc":"Databases","group":"databases"},
+
+
+{"group":"domains","desc":"Domains","items":[
+
+
+{"subtype":"img","onclick":"","acontent":"","height":"48","searchtext":"Domains Addon domain Addon Domains","width":"48","url":"\/panel\/indexpl.php?option=domains&ttt=-5815945509985899520","itemdesc":"Addon Domains","implements":"Domains_AddonDomains","type":"image","feature":"addondomains","base64_png_image":"","file":"addon_domains","group":"domains","itemorder":"1","target":"","imgtype":"icon"},
+
+{"itemorder":"3","target":"","imgtype":"icon","group":"domains","file":"subdomains","base64_png_image":"","feature":"subdomains","itemdesc":"Sub Domains","url":"\/panel\/indexpl.php?option=subdomains&ttt=-5815945509985899520","implements":"Domains_SubDomains","type":"image","width":"48","searchtext":"Subdomains domain","height":"48","acontent":"","onclick":"","subtype":"img"},
+
+{"height":"48","width":"48","searchtext":"Aliases Parked domains domain","onclick":"","subtype":"img","acontent":"","file":"aliases","imgtype":"icon","target":"","itemorder":"4","group":"domains","type":"image","url":"\/panel\/indexpl.php?option=parked&ttt=-5815945509985899520","itemdesc":"Aliases (Parked Domains)","base64_png_image":"","feature":"parkeddomains"},
+
+{"feature":"redirects","base64_png_image":"","url":"\/panel\/indexpl.php?option=redirect&ttt=-5815945509985899520","itemdesc":"Redirects","type":"image","group":"domains","target":"","itemorder":"5","imgtype":"icon","file":"redirects","acontent":"","subtype":"img","onclick":"","searchtext":"Redirects rewrite modrewrite","width":"48","height":"48"}]},
+
+
+{"desc":"Email","group":"email","items":[
+	{"acontent":"","subtype":"img","onclick":"","searchtext":"email accounts mail pop imap smtp Email Accounts","width":"48","height":"48","feature":"popaccts","base64_png_image":"","url":"\/panel\/indexpl.php?option=emailaccounts&ttt=-5815945509985899520","itemdesc":"Email Accounts","type":"image","implements":"Email_Accounts","group":"email","target":"","itemorder":"1","imgtype":"icon","file":"accounts"},
+
+{"base64_png_image":"","feature":"forwarders || emaildomainfwd","type":"image","implements":"Email_Forwarders","url":"\/panel\/indexpl.php?option=emailforwarder&ttt=-5815945509985899520","itemdesc":"Forwarders","imgtype":"icon","itemorder":"3","target":"","group":"email","file":"forwarders","acontent":"","onclick":"","subtype":"img","width":"48","searchtext":"Forwarders forward","height":"48"},
+
+{"height":"48","width":"48","searchtext":"MX Entry dns","onclick":"","subtype":"img","acontent":"","file":"mx_entry","imgtype":"icon","itemorder":"4","target":"","group":"email","implements":"Email_MX","type":"image","url":"\/panel\/indexpl.php?option=mxrecords&ttt=-5815945509985899520","itemdesc":"MX Entry","base64_png_image":"","feature":"changemx"},
+
+{"group":"email","imgtype":"icon","target":"","itemorder":"7","file":"advanced_zone_editor","feature":"zoneedit","base64_png_image":"","type":"image","url":"\/panel\/indexpl.php?option=spfrecords&ttt=-5815945509985899520","itemdesc":"SPF Records","searchtext":"spf record","width":"48","height":"48","acontent":"","subtype":"img","onclick":""},
+
+{"group":"email","imgtype":"icon","target":"_blank","itemorder":"7","file":"email_filters","feature":"email_filters","base64_png_image":"","type":"image","url":"http://185.27.134.244/roundcubemail/","itemdesc":"Webmail","searchtext":"webmail web mail email login","width":"48","height":"48","acontent":"","subtype":"img","onclick":""}]},
+
+
+
+
+{"items":[
+
+
+{"height":"48","width":"48","searchtext":"Account Statistics","onclick":"","subtype":"img","acontent":"","file":"awstats","imgtype":"icon","itemorder":"1","target":"","group":"metrics","type":"image","url":"/panel/indexpl.php?option=stats2&ttt=-5815945509985899520","itemdesc":"Account Statistics","base64_png_image":"","feature":"webalizer"},
+
+{"subtype":"img","onclick":"","acontent":"","height":"48","searchtext":"Errors errorlog error log","width":"48","type":"image","url":"/panel/indexpl.php?option=errorlogchoose&ttt=-5815945509985899520","itemdesc":"Error Logs","feature":"errlog","base64_png_image":"","file":"errors","group":"metrics","imgtype":"icon","target":"","itemorder":"2"},
+
+{"subtype":"img","onclick":"","acontent":"","height":"48","searchtext":"php settings phpinfo php info","width":"48","type":"image","url":"/panel/indexpl.php?option=phpconfig&ttt=-5815945509985899520","itemdesc":"General PHP Info","feature":"errlog","base64_png_image":"","file":"php","group":"metrics","imgtype":"icon","target":"","itemorder":"2"},
+
+
+
+{"searchtext":"Access Logs","width":"47","height":"47","acontent":"","subtype":"img","onclick":"","group":"metrics","itemorder":"99","target":"","imgtype":"icon","file":"raw_access","feature":"rawlog","base64_png_image":"","url":"/panel/indexpl.php?option=accesslogchoose&ttt=-5815945509985899520","itemdesc":"Access Logs","type":"image"}
+
+
+],"desc":"Metrics","group":"metrics"},
+
+{"group":"security","desc":"Security","items":[
+
+{"searchtext":"IP Blocker ip deny manager","width":"48","height":"48","acontent":"","subtype":"img","onclick":"","group":"security","imgtype":"icon","target":"","itemorder":"2","file":"ip_blocker","feature":"ipdeny","base64_png_image":"","type":"image","url":"/panel/indexpl.php?option=deny_ip&ttt=-5815945509985899520","itemdesc":"IP Blocker"},
+
+{"file":"ssl_tls","target":"","itemorder":"3","imgtype":"icon","group":"security","itemdesc":"SSL\/TLS","url":"/panel/indexpl.php?option=ssl&ttt=-5815945509985899520","type":"image","base64_png_image":"","feature":"sslmanager","height":"48","width":"48","searchtext":"SSL\/TLS certificate key csr","onclick":"","subtype":"img","acontent":""}]},
+
+
+{"group":"software","desc":"Software","items":[
+
+
+
+{"onclick":"","subtype":"img","acontent":"","height":"47","width":"47","searchtext":"SiteBuilder","itemdesc":"SiteBuilder","url":"/panel/indexpl.php?option=sitebuilder&ttt=-5815945509985899520","type":"image","base64_png_image":"","feature":"sitereptile","file":"sitereptile","itemorder":"99","target":"","imgtype":"icon","group":"software"},
+
+{"file":"softaculous","group":"software","itemorder":"99","target":"_blank","imgtype":"icon","url":"/panel/indexpl.php?option=installer&ttt=-5815945509985899520 target = '_blank'","itemdesc":"Softaculous Apps Installer","type":"image","feature":"softaculous","base64_png_image":"","height":"47","searchtext":"Softaculous Apps Installer","width":"47","subtype":"img","onclick":"","acontent":""},
+
+{"file":"attracta_seotips","group":"software","itemorder":"99","target":"_blank","imgtype":"icon","url":"/panel/indexpl.php?option=seotools&ttt=-5815945509985899520 target = '_blank'","itemdesc":"SEO Tools","type":"image","feature":"attracta_seotips","base64_png_image":"","height":"47","searchtext":"Search engine seo tools","width":"47","subtype":"img","onclick":"","acontent":""},
+
+
+{"file":"multiphp_configuration","group":"software","itemorder":"99","target":"_blank","imgtype":"icon","url":"/panel/indexpl.php?option=phpchangeconfig&ttt=-5815945509985899520 target = '_blank'","itemdesc":"Alter PHP Config","type":"image","feature":"multiphp_configuration","base64_png_image":"","height":"47","searchtext":"change PHP options timezone","width":"47","subtype":"img","onclick":"","acontent":""},
+
+
+]},
+
+
+
+{"items":[
+
+{"height":"48","width":"48","searchtext":"Cron Jobs cronjob crontab edit","onclick":"","subtype":"img","acontent":"","file":"cron_jobs","target":"","itemorder":"2","imgtype":"icon","group":"advanced","url":"/panel/indexpl.php?option=cron&ttt=-5815945509985899520","itemdesc":"Cron Jobs","type":"image","implements":"Cron_Home","base64_png_image":"","feature":"cron"},
+
+{"height":"48","width":"48","searchtext":"CNAME records dns","onclick":"","subtype":"img","acontent":"","file":"simple_zone_editor","target":"","itemorder":"2","imgtype":"icon","group":"advanced","url":"/panel/indexpl.php?option=cnamerecords&ttt=-5815945509985899520","itemdesc":"CNAME Records","type":"image","implements":"Cron_Home","base64_png_image":"","feature":"cron"},
+
+
+{"subtype":"img","onclick":"","acontent":"","height":"48","searchtext":"Error Pages errorlog error_log error log","width":"48","url":"/panel/indexpl.php?option=errorpages&ttt=-5815945509985899520","itemdesc":"Error Pages","type":"image","feature":"errpgs","base64_png_image":"","file":"error_pages","group":"advanced","target":"","itemorder":"5","imgtype":"icon"},
+
+],"group":"advanced","desc":"Advanced"},
+
+{"items":[
+
+{"onclick":"","subtype":"img","acontent":"","height":"47","width":"47","searchtext":"Softaculous Apps Installer","type":"image","itemdesc":"Softaculous Apps Installer","url":"/panel/indexpl.php?option=installer&ttt=-5815945509985899520","base64_png_image":"","feature":"soft_div_icons","file":"soft_div_icons","imgtype":"icon","target":"_blank","itemorder":"10","group":"soft_div"}],"group":"soft_div","desc":"Softaculous Apps Installer"},
+
+{"items":[
+
+{"base64_png_image":"","feature":"cloudflare_performance","itemdesc":"Domain Types","url":"/panel/indexpl.php?option=tutorialdomain&ttt=-5815945509985899520","type":"image","itemorder":"12","target":"","imgtype":"icon","group":"support","file":"cloudflare_performance","acontent":"","onclick":"","subtype":"img","width":"47","searchtext":"Domain Types","height":"47"},
+
+
+{"base64_png_image":"","feature":"cloudflare_analytics","itemdesc":"Tutorials","url":"http://tutorials.securesignup.net/","type":"image","itemorder":"12","target":"_blank","imgtype":"icon","group":"support","file":"cloudflare_analytics","acontent":"","onclick":"","subtype":"img","width":"47","searchtext":"tutorials","height":"47"},
+
+{"base64_png_image":"","feature":"attracta_seotools","itemdesc":"Support Forum","url":"http://byet.net/","type":"image","itemorder":"12","target":"_blank","imgtype":"icon","group":"support","file":"attracta_seotools","acontent":"","onclick":"","subtype":"img","width":"47","searchtext":"Support Forum","height":"47"},
+
+{"base64_png_image":"","feature":"createticket","itemdesc":"Create New Ticket","url":"/panel/indexpl.php?option=supportnewticket&ttt-5815945509985899520","type":"image","itemorder":"12","target":"","imgtype":"icon","group":"support","file":"createticket","acontent":"","onclick":"","subtype":"img","width":"47","searchtext":"Create New Ticket","height":"47"},
+
+
+
+
+{"base64_png_image":"","feature":"createticket","itemdesc":"No Support Tickets","url":"no","type":"image","itemorder":"1","target":"","imgtype":"icon","group":"support","file":"ShowTickets","acontent":"","onclick":"","subtype":"img","width":"47","searchtext":"No Support Tickets","height":"47"},
+
+
+
+
+{"feature":"ShowTickets","base64_png_image":"","itemdesc":"Closed Support Tickets","url":"/panel/indexpl.php?option=supportclosedtickets&ttt=<-5815945509985899520","type":"image","group":"support","itemorder":"2","target":"","imgtype":"icon","file":"api_shell","acontent":"","subtype":"img","onclick":"","searchtext":"Closed Support Tickets","width":"47","height":"47"}],"desc":"Support","group":"support"}];
